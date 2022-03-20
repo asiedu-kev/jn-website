2ada46394dabacce963dc0796c35b78e9acaa655
GangloUlrich <gangloulrichluc@gmail.com>
2022-03-20 12:46:53 +0100

    Mis en place du menu du site

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,20 +7,18 @@
 
 
 <div class="menu-overlay" id="menu-overlay">
-    <span class="closebtn cursor-pointer" id="close-menu"> <i class="fas fa-times cursor-pointer fa-2x color-primary"></i> </span>
+    <span class="closebtn cursor-pointer" id="close-menu"> &times;</i> </span>
 
     <div class="container menu-overlay-content">
     <div> <nav class="nav flex-column">
-        <a href="{{ route('home')}}" class="nav-link menu-overlay-link">Home</a>
+        <a href="{{ route('home')}}" class="nav-link menu-overlay-link color-primary">Home</a>
         <a href="{{ route('get_started')}}" class="nav-link menu-overlay-link">Getting Started</a>
         <a href="{{ route('training')}}" class="nav-link menu-overlay-link" >Trainings</a>
         <a href="{{ route('ressources')}}" class="nav-link menu-overlay-link">Ressources</a>
 
     </nav></div>
 
-    <div>
         <p class="menu-overlay-title text-uppercase">JUnior Natabou</p>
-    </div>
 
     </div>
 </div>
